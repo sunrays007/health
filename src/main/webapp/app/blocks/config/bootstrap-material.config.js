25ec40653ece725bc88c30a5d3b2b521ae57c6d9
Karnati, Kiran <kirankarnati007@gmail.com>
2017-04-20 10:08:17 -0400

    changed to material design on the presentation

---
+++ src/main/webapp/app/blocks/config/bootstrap-material.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('healthApp')
+        .config(bootstrapMaterialDesignConfig);
+
+    bootstrapMaterialDesignConfig.$inject = [];
+
+    function bootstrapMaterialDesignConfig() {
+        $.material.init();
+
+    }
+})();
